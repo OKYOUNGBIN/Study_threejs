22c2a693fb2d8007d18686c3ace574cf3da29e59
dean <margintop3498@icloud.com>
2022-06-19 15:18:21 +0900

    평행이동 추가

--- src/entities/InputSystem.ts
+++ src/entities/InputSystem.ts
@@ -1,23 +1,58 @@
 import Entity from "~/internal/Entity";
 import * as THREE from 'three';
+import { Ground } from "~/entities/Ground";
+import { Player } from "~/entities/Player";
 
 export class InputSystem extends Entity {
   mousePosition = new THREE.Vector2();
   raycaster = new THREE.Raycaster();
 
   start() {
-    window.addEventListener('mousemove', this.onMouseMove);
+    window.addEventListener('pointerup', this.onMouseUp);
+    window.addEventListener('pointerdown', this.onMouseDown);
+    window.addEventListener('pointermove', this.onMouseMove);
+  }
+
+  updateMousePosition(e: MouseEvent) {
+    this.mousePosition.x = (e.clientX / window.innerWidth) * 2 - 1;
+    this.mousePosition.y = -(e.clientY / window.innerHeight) * 2 + 1;
+  }
+
+  getIntersects(position: THREE.Vector2) {
+    const defaultSystem = this.app.getDefaultSystem();
+    this.raycaster.setFromCamera(position, defaultSystem.camera);
+    return this.raycaster.intersectObjects(defaultSystem.scene.children);
+  }
+
+  onMouseDown = (e: MouseEvent) => {
+    this.updateMousePosition(e);
+
+    const intersects = this.getIntersects(this.mousePosition);
+    const ground = this.app.getEntities().find(it => it instanceof Ground) as Ground;
+    if (!ground) {
+      return;
+    }
+
+    const intersect = intersects.find(it => it.object === ground.plane);
+    if (!intersect) {
+      return;
+    }
+
+    const player = this.app.getEntities().find(it => it instanceof Player) as Player;
+    if (!player) {
+      return;
+    }
+
+    player.setTargetPosition(intersect.point);
+  }
+
+  onMouseUp = (e: MouseEvent) => {
+    this.updateMousePosition(e);
   }
 
   onMouseMove = (e: MouseEvent) => {
-    this.mousePosition.x = (e.clientX / window.innerWidth) * 2 - 1;
-    this.mousePosition.y = -(e.clientY / window.innerHeight) * 2 + 1;
+    this.updateMousePosition(e);
 
-    const defaultSystem = this.app.getDefaultSystem();
 
-    this.raycaster.setFromCamera(this.mousePosition, defaultSystem.camera);
-    const intersectObjects = this.raycaster.intersectObjects(defaultSystem.scene.children);
-
-    console.log(intersectObjects);
   }
 }
--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -1,7 +1,15 @@
 import Entity from "~/internal/Entity";
 import * as THREE from 'three';
 
+function angleBetween(a: THREE.Vector2, b: THREE.Vector2) {
+  const dx = b.x - a.x;
+  const dy = b.y - a.y;
+  return Math.atan2(dy, dx);
+}
+
 export class Player extends Entity {
+  targetPosition?: THREE.Vector3;
+
   geometry: THREE.BoxGeometry;
   material: THREE.MeshStandardMaterial;
   box: THREE.Mesh<THREE.PlaneGeometry, THREE.MeshStandardMaterial>;
@@ -15,5 +23,26 @@ export class Player extends Entity {
     this.box.castShadow = true;
 
     defaultSystem.scene.add(this.box);
+  }
+
+  update(delta: number) {
+    if (!this.targetPosition) {
+      return;
+    }
+
+    const angle = angleBetween(
+      new THREE.Vector2(this.box.position.x, this.box.position.y),
+      new THREE.Vector2(this.targetPosition.x, this.targetPosition.y),
+    );
+
+    const speed = 0.5 * delta;
+    this.box.position.setX(this.box.position.x + Math.cos(angle) * speed);
+    this.box.position.setY(this.box.position.y + Math.sin(angle) * speed);
+  }
+
+  setTargetPosition(position: THREE.Vector3) {
+    this.targetPosition = position;
+    this.targetPosition.setZ(this.box.position.z);
+    // this.box.position.set(position.x, position.y, this.box.position.z);
   }
 }
